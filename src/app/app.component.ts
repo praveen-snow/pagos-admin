e149b142d0d3c98ad611c6df3aaf910ee74d0c57
praveen kumar <praveen.kumar2@verizon.com>
2019-04-25 02:17:55 -0500

    removing consols

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  ngOnInit() {
+    if (environment.production) {
+      window.console.log = function () { };
+    }
+  }
 }
